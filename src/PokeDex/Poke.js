a0d373ffeae9692d75851694df5e637afef095e3
Daniel Vanegas <danieru1313@gmail.com>
2019-07-22 00:21:01 -0500

    Add empty string to alt to avoid warning

--- src/PokeDex/Poke.js
+++ src/PokeDex/Poke.js
@@ -63,7 +63,7 @@ const Poke = () => {
       <div className="main">
         <img className="title" src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png" alt="poke-logo" />
         <div className="poke-dex">
-          <img className="img" src={poke.pokeImg}  />
+          <img className="img" src={poke.pokeImg} alt="" />
           <div onClick={resetPoke} className="reset"></div>
           <input type="text" value={poke.pokeId} onChange={handleEdit} className="id" />
           <div className="info">
